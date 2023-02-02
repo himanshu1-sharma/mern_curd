75f69204d19111e123f8d95fbc7743cfd35f2cff
Himanshu <sharmagi1998@gmail.com>
2023-02-02 15:53:47 +0530

    reported user data post ans get complete

--- backend/controllers/report.controller.js
+++ backend/controllers/report.controller.js
@@ -28,3 +28,41 @@ exports.reportUser = async (req, res) => {
         })
     }
 }
+
+exports.getReportUser = async (req, res) => {
+    try {
+        const ReporteUserData = await reportModel.find().populate([
+            {
+                path: "reportedBy",
+                model: "User",
+                select: "name email phoneno",
+            },
+            {
+                path: "reportedUser",
+                model: "User",
+                select: "name email phoneno"
+            }
+           
+        ])
+        if (!ReporteUserData) return res.status(200).json({
+            errorcode: 1,
+            status: false,
+            message: "data not found",
+            data: null
+        })
+
+        return res.status(200).json({
+            errorcode: 0,
+            status: true,
+            message: "reported data found",
+            data: ReporteUserData
+        })
+    } catch (error) {
+        return res.status(200).json({
+            errorcode: 5,
+            status: false,
+            message: error.message,
+            data: null
+        })
+    }
+}
--- backend/routes/report.routes.js
+++ backend/routes/report.routes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const Controller = require("../controllers/report.controller")
 
 router.post("/report-user", Controller.reportUser)
+router.get("/get-report-user", Controller.getReportUser)
 
 
 module.exports = router
